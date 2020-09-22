985a3264b5ff40456fb50a816d15133bdfe35fc8
Фадеева Дарья Сергеевна <ds.fadeeva@expoforum.ru>
2020-09-22 17:14:12 +0300

    Refactor package

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,18 @@
+package ru.netology.manager;
+
+public class PostManager {
+
+    private Manager.PostManager[] posts;
+
+    public Manager.PostManager[] search(int ownerId, String domain, String query, boolean ownerOnly, int count, int offset) {
+
+        //TODO: add logic
+
+        return null;
+    }
+
+    public void delete (int ownerId, int postId) {
+
+        //TODO: add logic
+    }
+}

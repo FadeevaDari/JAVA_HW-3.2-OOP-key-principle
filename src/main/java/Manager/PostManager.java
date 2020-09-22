4ec8da814bc3da2f3e624035e0f2f92e66cdb6e0
Фадеева Дарья Сергеевна <ds.fadeeva@expoforum.ru>
2020-09-22 17:09:58 +0300

    Added package Manager

---
+++ src/main/java/Manager/PostManager.java
@@ -0,0 +1,17 @@
+package Manager;
+
+public class PostManager {
+    private PostManager[] posts;
+
+    public PostManager[] search(int ownerId, String domain, String query, boolean ownerOnly, int count, int offset) {
+
+        //TODO: add logic
+
+        return null;
+    }
+
+    public void delete (int ownerId, int postId) {
+
+        //TODO: add logic
+    }
+}
